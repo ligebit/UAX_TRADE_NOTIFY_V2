b03223cc8e301542a80f4f84ad99a5b720f1dcc2
Foton714 <fotonar714@gmail.com>
2020-06-22 09:18:15 +0300

    add dont sleep

--- index.js
+++ index.js
@@ -1,4 +1,25 @@
 const { BOT } = require('./BOT/BOT.js');
+const request = require('request');
+const http = require('http');
 
 
 var bot = new BOT();
+
+/*--- Код для того, что бы будить heroku ---*/
+const server = http.createServer((request, response) => {
+    response.end('Hello uax notify bot!');
+}).listen(process.env.PORT || 3000, (err) => {
+    if (err) {
+        return console.log('Ошибка сервера ', err)
+    }
+})
+
+function dontSleep() {
+    request("https://api.ethplorer.io/getTokenInfo/", () => {
+        console.log('Не спать');
+    });
+}
+
+SetInterval(dontSleep, 1000*60*25);
+
+/*--- --- ---- ---*/
